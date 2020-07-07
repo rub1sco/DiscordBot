fafc8d5a635d15e26ee9239a6876b23a8e57dc2e
Tom <tom.lucero@me.com>
2020-07-07 07:46:08 -0600

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // set up variables
 require('dotenv').config();
 const Discord = require('discord.js');
-const {prefix, token} = require('../utils/config.json');
+// const {prefix, token} = require('../utils/config.json');
 var functions = require('./functions.js');
 const client = new Discord.Client();
 
@@ -19,7 +19,7 @@ client.login(process.env.BOT_TOKEN);
 // on can trigger multiple times
 // receives every message in channel.
 client.on('message', message => {
-  if(!message.content.startsWith(prefix) || message.author.bot){return;}
+  if(!message.content.startsWith(process.env.PREFIX) || message.author.bot){return;}
 
   // splits by space ' ' and does not error with multiple spaces.
   const args = message.content.slice(prefix.length).split(/ +/);
