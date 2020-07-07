4e4964739109c6632c1375bcabce900795f908b7
Tom <tom.lucero@me.com>
2020-07-07 09:41:20 -0600

    tidying index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const Discord = require('discord.js');
 // const {prefix, token} = require('../utils/config.json');
 var functions = require('./functions.js');
 const client = new Discord.Client();
-
+const prefix = process.env.PREFIX;
+const token = process.env.BOT_TOKEN;
 
 // launches node server
 client.once('ready', () => {
@@ -13,16 +14,16 @@ client.once('ready', () => {
 
 // logs into server with token.
 // TODO make token secret before git push or heroku
-client.login(process.env.BOT_TOKEN);
+client.login(token);
 
 
 // on can trigger multiple times
 // receives every message in channel.
 client.on('message', message => {
-  if(!message.content.startsWith(process.env.PREFIX) || message.author.bot){return;}
+  if(!message.content.startsWith(prefix) || message.author.bot){return;}
 
   // splits by space ' ' and does not error with multiple spaces.
-  const args = message.content.slice(process.env.PREFIX.length).split(/ +/);
+  const args = message.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
 
   // handles all incoming events and passes to functions.js
